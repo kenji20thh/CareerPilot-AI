f6ebc97ed3a341ee44a5bf8cd7c9d845ccd8dd8a
kenji20thh <amineelayachi22@outlook.com>
2026-08-16 21:02:18 +0200

    verify method

--- backend/handlers/profile.go
+++ backend/handlers/profile.go
@@ -3,5 +3,7 @@ package handlers
 import "net/http"
 
 func uploadCV(w http.ResponseWriter, r *http.Request) {
-
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+	}
 }
