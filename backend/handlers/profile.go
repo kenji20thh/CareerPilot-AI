89ec0f29243a4eab99f4f0ac5fd9626471dd6cfc
kenji20thh <amineelayachi22@outlook.com>
2026-08-16 21:03:35 +0200

    parsing form

--- backend/handlers/profile.go
+++ backend/handlers/profile.go
@@ -6,4 +6,10 @@ func uploadCV(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 	}
+
+	err := r.ParseMultipartForm(10 << 20)
+	if err != nil {
+		http.Error(w, "Could Not Parse From", http.StatusBadRequest)
+	}
+
 }
