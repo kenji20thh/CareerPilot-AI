14726a8f9b543e6ed612df909ba9f362955ee743
kenji20thh <amineelayachi22@outlook.com>
2026-08-16 21:05:03 +0200

    get cv

--- backend/handlers/profile.go
+++ backend/handlers/profile.go
@@ -5,11 +5,19 @@ import "net/http"
 func uploadCV(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
 	}
 
 	err := r.ParseMultipartForm(10 << 20)
 	if err != nil {
 		http.Error(w, "Could Not Parse From", http.StatusBadRequest)
+		return
+	}
+
+	file, header, err := r.FormFile("cv")
+	if err != nil {
+		http.Error(w, "CV file is required", http.StatusBadRequest)
+		return
 	}
 
 }
