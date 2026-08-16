a41383f123cc1f4ac47193627fb0c6af3f3c7697
kenji20thh <amineelayachi22@outlook.com>
2026-08-16 04:38:10 +0200

    healthhandler

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 )
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{
+		"status": "ok",
+	})
 
 }
