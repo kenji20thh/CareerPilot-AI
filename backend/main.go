513667e56667379e626583443824fed22da87f83
kenji20thh <amineelayachi22@outlook.com>
2026-08-21 00:26:03 +0200

    withCORS headers allowed

--- backend/main.go
+++ backend/main.go
@@ -17,6 +17,7 @@ func withCORS(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Acess-Control-Allow-Origin", "http://localhost:8080")
 		w.Header().Set("Acess-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Acess-Control-Allow-Headers", "Content-Type")
 	}
 }
 
