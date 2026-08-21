7c8a00170ae0d9cced9c5e53b5d09eef98db1998
kenji20thh <amineelayachi22@outlook.com>
2026-08-21 03:53:47 +0200

    fixing route bug

--- backend/main.go
+++ backend/main.go
@@ -15,7 +15,7 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 
 func withCORS(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
