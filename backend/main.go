e7060a39427d09f6a9556d76faa34780216dbfdc
kenji20thh <amineelayachi22@outlook.com>
2026-08-21 03:54:09 +0200

    correcting main.go

--- backend/main.go
+++ backend/main.go
@@ -15,9 +15,9 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 
 func withCORS(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Acess-Control-Allow-Origin", "http://localhost:8080")
+		w.Header().Set("Acess-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Acess-Control-Allow-Headers", "Content-Type")
 
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusOK)
