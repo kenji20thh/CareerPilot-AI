eb371199873ab5f771fdb3729cd5eede296eb475
kenji20thh <amineelayachi22@outlook.com>
2026-08-22 02:41:23 +0200

    reading .env file

--- backend/db/db.go
+++ backend/db/db.go
@@ -1,5 +1,18 @@
 package db
 
-import "github.com/jackc/pgx/v5/pgxpool"
+import (
+	"log"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+)
 
 var Pool *pgxpool.Pool
+
+func Connect() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found, relying on system environment variables")
+
+	}
+}
