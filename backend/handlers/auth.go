b47b3cf5837feb1175ba203c03c34c7dbc3de6be
kenji20thh <amineelayachi22@outlook.com>
2026-08-23 23:54:31 +0200

    func Signup

--- backend/handlers/auth.go
+++ backend/handlers/auth.go
@@ -1,6 +1,16 @@
 package handlers
 
+import "net/http"
+
 type SignupRequest struct {
 	Email    string `json:"email`
 	Password string `json:"password"`
+}
+
+func Singup(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
 }
