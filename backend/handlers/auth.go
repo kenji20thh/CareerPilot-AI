bad092926519e011e9bca5363bc6c653c5db6e0d
kenji20thh <amineelayachi22@outlook.com>
2026-08-24 03:30:41 +0200

    enconding userId and token in json

--- backend/handlers/auth.go
+++ backend/handlers/auth.go
@@ -56,4 +56,9 @@ func Singup(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("content-type", "application/json")
+	json.NewEncoder(w).Encode(map[string]interface{}{
+		"success": true,
+		"userId":  userID,
+		"token":   token,
+	})
 }
