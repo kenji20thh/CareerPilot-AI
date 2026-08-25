0e32a472e637b1c336311525144bd26829ff65e3
kenji20thh <amineelayachi22@outlook.com>
2026-08-25 03:22:50 +0200

    checking for auth header

--- backend/middleware/auth.go
+++ backend/middleware/auth.go
@@ -8,6 +8,10 @@ const userIDKey contextKey
 
 func RequireAuth(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			http.Error(w, "Missing Authorization Header", http.StatusUnauthorized)
+			return
+		}
 	}
 }
