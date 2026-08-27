80a9ba3ee5c069822edec6b2219666f951de0212
kenji20thh <amineelayachi322@gmail.com>
2026-08-27 09:04:12 +0100

    fixed middleware auth

--- backend/main.go
+++ backend/main.go
@@ -19,7 +19,7 @@ func withCORS(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusOK)
